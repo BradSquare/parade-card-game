83b8cf7f49982b808bf47b7ab326beb637a8788a
BradSquare <gohbradley@gmail.com>
2025-03-05 15:00:16 +0800

    Test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // Start a game with 2 players for simplicity
         ParadeGame game = new ParadeGame(2);
-        game.startGame();
+        game.startGame(); 
     }
 }
