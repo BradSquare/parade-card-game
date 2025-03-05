63ebe008b5156e534f732501fa8089be830c7a4a
BradSquare <gohbradley@gmail.com>
2025-03-05 13:08:05 +0800

    Card

---
+++ src/Card.java
@@ -0,0 +1,18 @@
+public class Card {
+
+    private int value;
+    private String colour;
+
+    public Card(int value, String colour) {
+        this.value = value;
+        this.colour = colour;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+}
